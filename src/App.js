c9a3f44bcd326b2cf9e7eebd50e2ad6a749f2e9e
Nicholas <nicholasweecw@gmail.com>
2023-01-04 17:07:53 +0800

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Clock from "./Components/Clock";
 
 function App() {
+  const date = new Date();
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const day =
+    days[`${date.getDay() - 1}`] +
+    " " +
+    date.getDate() +
+    "/" +
+    `${date.getMonth() + 1}` +
+    "/" +
+    date.getFullYear();
+  /*const time =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(); */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Clock App</h1>
+      <h2>{day}</h2>
+      <Clock></Clock>
     </div>
   );
 }
---
+++ src/Components/Clock.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function Clock() {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  return <h2>{clockState}</h2>;
+}
+
+export default Clock;
